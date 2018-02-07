146213a5df39827492a8b305450ec198cc67eec6
pingyi Luo <pingyiluo@gmail.com>
2018-02-08 00:03:05 +0800

    update

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -4,9 +4,13 @@ import { User } from '../entity/User';
 
 export default (p: Promise<Connection>) => {
     return async function home(ctx: Context, next: () => Promise<any>) {
+      
         let connection: Connection = await p;
         const userRepository = await connection.getRepository(User);
         let users = await userRepository.find();
         ctx.body = users;
     }
 }
+
+
+
