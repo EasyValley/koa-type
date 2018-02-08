293cc8baa0de2922513dcd3650471c33558b404a
pingyi Luo <pingyiluo@gmail.com>
2018-02-08 15:21:46 +0800

    update

--- src/controller/errorHandler.ts
+++ src/controller/errorHandler.ts
@@ -1,10 +1,13 @@
 import { Context } from 'koa';
+import * as moment from 'moment';
 
 export async function errorHandler(ctx: Context, next: () => Promise<any>) {
     try {
         await next();
     } catch (e) {
-        ctx.logger.error(e);
+
+        let time = moment().format('YYYY-MM-DD HH:mm:ss');
+        ctx.logger.error(time, e);
         ctx.body = {
             code: 500,
             message: e.message
--- src/util/winstonLogger.ts
+++ src/util/winstonLogger.ts
@@ -1,15 +1,18 @@
 import * as winston from 'winston';
 import * as path from 'path';
+import * as fs from 'fs';
 import { Context } from 'koa';
 
 
 function winstonLogger() {
     return async (ctx: Context, next: () => Promise<any>) => {
+        let filename = path.join(__dirname, '../../../logs/info.log');
+
         if (!ctx.logger) {
             ctx.logger = new (winston.Logger)({
                 transports: [
                     new (winston.transports.Console)(),
-                    new (winston.transports.File)({ filename: path.join(__dirname, '../../logs/info.log') })
+                    new (winston.transports.File)({ filename })
                 ]
             });
         }
